51bdec70e9d35c198e02feaab47dde4e7fcd730f
Mule <myoni@gmai7634l.com>
2020-09-30 21:37:11 +0300

    download1.2

--- src/components/pages/Display.js
+++ src/components/pages/Display.js
@@ -1,25 +1,9 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { MDBRow,MDBAnimation } from "mdbreact";
+import { MDBRow, MDBAnimation } from "mdbreact";
 import Card from "./card";
-import axios from "axios";
-//
-class Display extends Component {
 
-  downloadImage(imageToBeDownloaded) {
-    axios({
-      url: imageToBeDownloaded,
-      method: 'GET',
-      responseType: 'blob',
-    }).then((response) => {
-      let fileURL = window.URL.createObjectURL(new Blob([response.data]));
-      let fileLink = document.createElement('a');
-      fileLink.href = fileURL;
-      fileLink.setAttribute('download', imageToBeDownloaded);
-      document.body.appendChild(fileLink);
-      fileLink.click();
-    });
-  }
+class Display extends Component {
   render() {
     let imageListContent;
 
@@ -33,25 +17,16 @@ class Display extends Component {
               // <a href={img.main} data-lightbox="mygallery" data-title={img.by}>
               //    </a>
               <div className="img-container" >
-             <MDBAnimation reveal type="fadeInLeft">    
-                <Card
-                  key={key}
-                  small={img.tumb}
-                  large={img.main}
-                  user={img.by}
-                  id={img.id}
-                />
-            </MDBAnimation>
-                <div className="download"> 
-                <div className="download-btn"
-                  onClick={() => this.downloadImage(img.main)}
-                >
-                  <i className="ion-arrow-down-a"> </i>
-                </div>
-                </div>
-              
+                <MDBAnimation reveal type="fadeInLeft">
+                  <Card
+                    key={key}
+                    small={img.tumb}
+                    large={img.main}
+                    user={img.by}
+                    id={img.id}
+                  />
+                </MDBAnimation>
               </div>
-
             ))}
           </div>
         </div>
--- src/components/pages/card.js
+++ src/components/pages/card.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 class Card extends Component {
   constructor() {
     super();
     this.state = {
       img: ""
     };
+  }
+
+  downloadImage(imageToBeDownloaded) {
+    axios({
+      url: imageToBeDownloaded,
+      method: 'GET',
+      responseType: 'blob',
+    }).then((response) => {
+      let fileURL = window.URL.createObjectURL(new Blob([response.data]));
+      let fileLink = document.createElement('a');
+      fileLink.href = fileURL;
+      fileLink.setAttribute('download', imageToBeDownloaded);
+      document.body.appendChild(fileLink);
+      fileLink.click();
+    });
   }
   componentWillMount() {
     this.setState({ img: this.props.large });
@@ -17,7 +32,20 @@ class Card extends Component {
   render(props) {
     const { user } = this.props;
     console.log(user);
-    return <img  src={this.state.img} alt={user} />;
+    return (
+      <div>
+        <img src={this.state.img} alt={user} />
+        <div className="download">
+          <div className="download-btn"
+            onClick={() => this.downloadImage(this.props.large)}
+          >
+            <i className="ion-arrow-down-a"> </i>
+          </div>
+        </div>
+      </div>
+    )
+
+
   }
 }
 
